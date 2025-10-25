4a58cd497d52f2bd4eb73b51532045550398d3be
Andy Garcia <andygarcia.oy@gmail.com>
2025-10-24 23:30:54 -0300

    update

--- microservices_ingredients/src/main/java/com/agarcia/microservices_ingredients/MicroservicesIngredientsApplication.java
+++ microservices_ingredients/src/main/java/com/agarcia/microservices_ingredients/MicroservicesIngredientsApplication.java
@@ -2,7 +2,9 @@ package com.agarcia.microservices_ingredients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicesIngredientsApplication {
 
