da6649166c71ec401ec3fcfc8514e1454b470051
Andy Garcia <andygarcia.oy@gmail.com>
2025-10-27 22:33:30 -0300

    create commons microservices

---
+++ commons_microservices/src/test/java/com/agarcia/commons_microservices/CommonsMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agarcia.commons_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
