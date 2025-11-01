bcaa887c36f0d389c33f97cc659b9380ae97b21a
Andy Garcia <andygarcia.oy@gmail.com>
2025-11-01 15:33:13 -0300

    common controller create, ingredientscontroller modificado como clase hija

---
+++ commons_microservices/src/main/java/com/agarcia/commons_microservices/services/controller/CommonController.java
@@ -0,0 +1,44 @@
+package com.agarcia.commons_microservices.services.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.agarcia.commons_microservices.services.CommonService;
+
+public class CommonController<E, S extends CommonService<E>> {
+
+    @Autowired
+    protected S service;
+
+    @GetMapping
+    public ResponseEntity<?> list(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> ver(@PathVariable Long id){
+        Optional<E> o = service.findById(id);  
+        if (o.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        
+        return ResponseEntity.ok(o.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody E entity){
+        E ingredientsDB = service.save(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ingredientsDB);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- microservices_ingredients/src/main/java/com/agarcia/microservices_ingredients/controller/IngredientsController.java
+++ microservices_ingredients/src/main/java/com/agarcia/microservices_ingredients/controller/IngredientsController.java
@@ -5,49 +5,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.agarcia.commons_microservices.services.controller.CommonController;
 import com.agarcia.microservices_ingredients.persistence.models.IngredientsEntity;
 import com.agarcia.microservices_ingredients.services.IngredientsService;
 
 
 @RestController
-public class IngredientsController {
-
-    @Autowired
-    private IngredientsService ingredientsService;
-
-    @GetMapping
-    public ResponseEntity<?> list(){
-        return ResponseEntity.ok().body(ingredientsService.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<?> ver(@PathVariable Long id){
-        Optional<IngredientsEntity> o = ingredientsService.findById(id);  
-        if (o.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-        
-        return ResponseEntity.ok(o.get());
-    }
-
-    @PostMapping
-    public ResponseEntity<?> crear(@RequestBody IngredientsEntity ingredientsEntity){
-        IngredientsEntity ingredientsDB = ingredientsService.save(ingredientsEntity);
-        return ResponseEntity.status(HttpStatus.CREATED).body(ingredientsDB);
-    }
+public class IngredientsController extends CommonController<IngredientsEntity, IngredientsService> {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> editar(@RequestBody IngredientsEntity ingredientsEntity, 
     @PathVariable Long id){
-        Optional<IngredientsEntity> o = ingredientsService.findById(id);
+        Optional<IngredientsEntity> o = service.findById(id);
 
         if (o.isEmpty()) {
             ResponseEntity.notFound().build();
@@ -58,13 +32,7 @@ public class IngredientsController {
         ingredientesDB.setQuantity(ingredientsEntity.getQuantity());
         ingredientesDB.setCost(ingredientesDB.getCost());
         ingredientesDB.setStock(ingredientesDB.getStock());
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(ingredientsService.save(ingredientesDB));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Long id){
-        ingredientsService.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.save(ingredientesDB));
     }
 
 }
