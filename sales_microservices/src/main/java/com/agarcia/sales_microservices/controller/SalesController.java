f33fa6f1683967b74882e218cf61f85f7cab42c2
mrendyg <andygarcia.oy@gmail.com>
2025-11-19 21:15:10 -0300

    Service Sales and put by Sales Created

---
+++ sales_microservices/src/main/java/com/agarcia/sales_microservices/controller/SalesController.java
@@ -0,0 +1,32 @@
+package com.agarcia.sales_microservices.controller;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agarcia.commons_microservices.services.controller.CommonController;
+import com.agarcia.sales_microservices.persistence.models.SalesEntity;
+import com.agarcia.sales_microservices.service.SalesService;
+
+@RestController
+public class SalesController extends CommonController<SalesEntity, SalesService>{
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody SalesEntity sales, @PathVariable Long id){
+        Optional<SalesEntity> o = service.findById(id);
+        
+        if(!o.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        SalesEntity salesDB = o.get();
+        salesDB.setSellerId(sales.getSellerId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(salesDB));
+
+    }
+}
---
+++ sales_microservices/src/main/java/com/agarcia/sales_microservices/service/SalesService.java
@@ -0,0 +1,8 @@
+package com.agarcia.sales_microservices.service;
+
+import com.agarcia.commons_microservices.services.CommonService;
+import com.agarcia.sales_microservices.persistence.models.SalesEntity;
+
+public interface SalesService extends CommonService<SalesEntity> {
+    
+}
